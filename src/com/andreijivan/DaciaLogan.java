52328fc50ec0ae5872f980569d0d319b1546be1a
Andrei <andrei_jivian@yahoo.com>
2020-01-21 16:56:20 +0200

    commit DaciaLogan methods from interface

--- src/com/andreijivan/DaciaLogan.java
+++ src/com/andreijivan/DaciaLogan.java
@@ -33,4 +33,19 @@ public class DaciaLogan extends Dacia {
     public void setTyreSize(int tyreSize) {
         this.tyreSize = tyreSize;
     }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void drive(int km) {
+
+    }
 }
